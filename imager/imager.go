c65ca8fd4be796addbb7cc8778c6266b03425f3b
Пётр Зимцев <zimtsev.p@gmail.com>
2019-04-17 18:06:24 +0300

    clipping fixed image

--- imager/imager.go
+++ imager/imager.go
@@ -48,22 +48,6 @@ func Init() {
 	}
 }
 
-// func tryFour() {
-// 	var points []map[int]int
-// 	var x0, y0 int
-// 	var radius int
-// 	for x := 0; x < radius*2; x++ {
-// 		for y := 0; y < radius*2; y++ {
-// 			if math.Pow(float64(x-x0), 2)+math.Pow(float64(y-y0), 2) == math.Pow(float64(radius), 2) {
-// 				var buf map[int]int
-// 				buf[x] = y
-// 				points = append(points, buf)
-// 				points[x] = y
-// 			}
-// 		}
-// 	}
-// }
-
 func circleRadius(r *int) *int {
 	x := float64(config.ImageWidth / 2)
 	y := float64(config.ImageHeight / 2)
@@ -73,11 +57,12 @@ func circleRadius(r *int) *int {
 
 func rounding(figure *image.RGBA) {
 
-	сolorLeftTopAngle := color.RGBA{11, 188, 254, 255}
-	сolorRightTopAngle := color.RGBA{0, 255, 0, 255}
-	colorLeftBottom := color.RGBA{102, 255, 0, 255}
-	colorRightBottom := color.RGBA{255, 3, 62, 255}
-
+	// сolorLeftTopAngle := color.RGBA{11, 188, 254, 255}
+	// сolorRightTopAngle := color.RGBA{0, 255, 0, 255}
+	// colorLeftBottom := color.RGBA{102, 255, 0, 255}
+	// colorRightBottom := color.RGBA{255, 3, 62, 255}
+	// colorTest := color.RGBA{255, 3, 62, 255}
+	colorTest := color.Transparent
 	radius := 110
 	if radius > 200/2 {
 		radius = 200 / 2
@@ -91,21 +76,20 @@ func rounding(figure *image.RGBA) {
 
 	x, y, dx, dy := radius, 0, 1, 1
 	err := dx - (int(float64(radius) * 1.35))
-	log.Println("Center: x0", x0, "y0", y0)
 
 	for x >= y {
 
-		figure.Set(x0-y, y0-x, сolorLeftTopAngle) // левый верх верхней части
-		figure.Set(x0-x, y0-y, сolorLeftTopAngle) // левый низ верхней части
+		figure.Set(x0-y, y0-x, colorTest) // левый верх верхней части
+		figure.Set(x0-x, y0-y, colorTest) // левый низ верхней части
 
-		figure.Set(x0-x, y0+y+shiftDown, colorLeftBottom) // левый верх нижней части
-		figure.Set(x0-y, y0+x+shiftDown, colorLeftBottom) // левый низ нижней части
+		figure.Set(x0-x, y0+y+shiftDown, colorTest) // левый верх нижней части
+		figure.Set(x0-y, y0+x+shiftDown, colorTest) // левый низ нижней части
 
-		figure.Set(x0+y+shiftRight, y0-x, сolorRightTopAngle) // правый угол верхняя половина половины
-		figure.Set(x0+x+shiftRight, y0-y, сolorRightTopAngle) // правый угол нижняя половина половины
+		figure.Set(x0+y+shiftRight, y0-x, colorTest) // правый угол верхняя половина половины
+		figure.Set(x0+x+shiftRight, y0-y, colorTest) // правый угол нижняя половина половины
 
-		figure.Set(x0+x+shiftRight, y0+y+shiftDown, colorRightBottom) // низ право верхняя часть
-		figure.Set(x0+y+shiftRight, y0+x+shiftDown, colorRightBottom) // низ право нижняя часть
+		figure.Set(x0+x+shiftRight, y0+y+shiftDown, colorTest) // низ право верхняя часть
+		figure.Set(x0+y+shiftRight, y0+x+shiftDown, colorTest) // низ право нижняя часть
 
 		if err <= 0 {
 			y++
@@ -123,73 +107,70 @@ func rounding(figure *image.RGBA) {
 	if radius == 200/2 {
 		bottomPoint = radius
 	} else {
-		bottomPoint = 200 - radius - 1
+		bottomPoint = 200 - radius
 	}
+	log.Println(bottomPoint)
 
-	clippingLeft(figure, сolorLeftTopAngle, 0)
-	clippingLeft(figure, colorLeftBottom, bottomPoint)
-	clippingRight(figure, сolorRightTopAngle, 0)
-	clippingRight(figure, colorRightBottom, bottomPoint)
+	// clippingLeft(figure, colorTest)
+	clippingRight(figure, colorTest)
 }
 
-func clippingLeft(figure *image.RGBA, col color.Color, yStart int) {
-	var (
-		finish bool
-	)
+func clippingLeft(figure *image.RGBA, col color.Color) {
 
-	colorF := color.RGBA{0, 255, 127, 255}
-	// colorF := color.Transparent
+	// colorF := color.RGBA{251, 7, 217, 255}
+	colorF := color.Transparent
 
-	for y := yStart; y < 200; y++ {
-		for x := 0; x < 100; x++ {
-			if figure.At(x, y) == col && x == 0 {
+	for y := 0; y < 200; y++ {
+		for x := 0; x < 200/2; x++ {
+			cr, cg, cb, ca := col.RGBA()
+			fr, fg, fb, fa := figure.At(x, y).RGBA()
+
+			if fr == cr && fg == cg && fb == cb && fa == ca && x == 0 {
 				figure.Set(x, y, colorF)
-				if figure.At(x, y+1) != col && yStart == 0 {
-					finish = true
-				}
+				fr, fg, fb, fa = figure.At(x, y+1).RGBA()
 				break
-			} else if figure.At(x, y) == col {
+			} else if fr == cr && fg == cg && fb == cb && fa == ca {
 				figure.Set(x, y, colorF)
-				if figure.At(x+1, y) != col {
+				fr, fg, fb, fa = figure.At(x+1, y).RGBA()
+				if fr != cr && fg != cg && fb != cb && fa != ca {
 					break
 				}
 			} else {
 				figure.Set(x, y, colorF)
 			}
 		}
-		if finish {
-			break
-		}
 	}
 }
 
-func clippingRight(figure *image.RGBA, col color.Color, yStart int) {
-	var (
-		finish bool
-	)
-	colorF := color.RGBA{0, 255, 127, 255}
-	// colorF := color.Transparent
+func clippingRight(figure *image.RGBA, col color.Color) {
 
-	for y := yStart; y < 200; y++ {
-		for x := 199; x > 100; x-- {
-			if figure.At(x, y) == col && x == 199 {
+	// colorF := color.RGBA{251, 7, 217, 255}
+	colorF := color.Transparent
+
+	for y := 0; y < 200; y++ {
+		for x := 200; x > 200/2; x-- {
+
+			cr, cg, cb, ca := col.RGBA()
+			fr, fg, fb, fa := figure.At(x, y).RGBA()
+			if x > 40 && x < 50 {
+				log.Println("x", x, "y", y)
+				log.Println("cr", cr, "cg", cg, "cb", cb, "ca", ca)
+				log.Println("fr", fr, "fg", fg, "fb", fb, "fa", fa)
+			}
+			if fr == cr && fg == cg && fb == cb && fa == ca && x == 199 {
 				figure.Set(x, y, colorF)
-				if figure.At(x, y+1) != col && yStart == 0 {
-					finish = true
-				}
 				break
-			} else if figure.At(x, y) == col {
+			} else if fr == cr && fg == cb && fb == cb && fa == ca {
 				figure.Set(x, y, colorF)
-				if figure.At(x-1, y) != col {
+				fr, fg, fb, fa = figure.At(x, y+1).RGBA()
+				if fr != cr && fg != cg && fb != cb && fa != ca {
 					break
 				}
 			} else {
 				figure.Set(x, y, colorF)
 			}
 		}
-		if finish {
-			break
-		}
+
 	}
 }
 
